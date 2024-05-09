0302b5c70ca4ddec556a779560f43cb1b0478043
raghul-001 <raghul17m@gmail.com>
2024-05-09 10:43:39 +0530

    find a day of week in java using LocalDate method

--- FindDayOfWeek.java
+++ FindDayOfWeek.java
@@ -1,1 +1,20 @@
+
 import java.util.*;
+import java.time.*;
+class FindDayOfWeek{
+    public static void main(String[] args) {
+    int date,month,year;
+
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("enter a Date : ");
+        date = scanner.nextInt();
+        System.out.println("enter a Month : ");
+        month = scanner.nextInt();
+        System.out.println("enter a Year : ");
+        year = scanner.nextInt();
+
+       LocalDate dd = LocalDate.of(year, month, date);
+        System.out.println(dd.getDayOfWeek());
+
+    }
+}
